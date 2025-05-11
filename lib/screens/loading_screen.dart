d1b3ed3ed7ea66d7970cd2fd249023b9e5bc9527
23091397101_Roberto Christian7 <149355428+RobertoChristian@users.noreply.github.com>
2025-05-11 20:34:25 +0700

    Update loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -14,17 +14,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const HomeScreen()),
+      );
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black, // Background hitam
       body: SizedBox.expand(
-        child: FittedBox(
-          fit: BoxFit.cover,
-          child: Image.asset('assets/logo.png'),
+        child: Image.asset(
+          'assets/logo.png',
+          fit: BoxFit.cover, // Logo memenuhi layar
         ),
       ),
     );
